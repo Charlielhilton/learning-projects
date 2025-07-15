109aa4ff65ec9dcbe470b317693837585b8956fe
Charlielhilton <charlottelhilton@gmail.com>
2025-07-15 20:11:08 +0100

    Project 1 - Static Portfolio

---
+++ Project 1 - Static Developer Portfolio/script.js
@@ -0,0 +1,4 @@
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("This is a static portfolio so this button doesn't do anything! Thanks for clicking though");
+});
